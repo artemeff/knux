2413f3685a1a25bdebe306b8e46381f560688cd9
Yuri Artemev <i@artemeff.com>
2019-09-13 01:49:12 +0300

    prepare for publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Knux.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
     ]
   end
 
@@ -19,7 +21,19 @@ defmodule Knux.MixProject do
 
   defp deps do
     [
-      {:connection, "~> 1.0.4"}
+      {:connection, "~> 1.0.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description do
+    "Library for dealing with data structures"
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/artemeff/knux"}
     ]
   end
 end
